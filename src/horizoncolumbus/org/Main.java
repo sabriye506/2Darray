9002e59f489bf513877ce6862f9e1974b89466c0
Sabriyes Project <student.sbr.mohamed7@horizoncolumbus.org>
2021-02-12 18:06:10 -0500

    Initial commit

---
+++ src/horizoncolumbus/org/Main.java
@@ -0,0 +1,15 @@
+package horizoncolumbus.org;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int arr[][] = {{8, 7, 9},{3,7,5},{7,4,4}};
+
+	    for (int i = 0; i < 3; i++){
+	        for (int j = 0; j < 3; j++){
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
